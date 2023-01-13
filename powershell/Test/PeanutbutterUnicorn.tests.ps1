2a1ee698e0c8b8e7d195b83fed609391263cec0d
Jonathan Zollinger <jonathan.zollinger@outlook.com>
2023-01-13 15:24:41 -0700

    isolate  pwsh and nvim dockerfiles

---
+++ powershell/Test/PeanutbutterUnicorn.tests.ps1
@@ -0,0 +1,49 @@
+# try {
+#     $ErrorActionPreference = 'Silently-Continue'
+#     Remove-Module PeanutbutterUnicorn
+# }catch {
+#     Write-Verbose "ignoring failure of 'Remove-Module Peanutbutterunicorn"
+# }
+# $ErrorActionPreference = 'Continue'
+# $env:PSModulePath = "$(Split-Path $PWD -Parent);$($env:PSModulePath)"
+# RefreshEnv
+using module "../PeanutbutterUnicorn.psm1"
+
+Describe 'Join-Vlab' {
+
+    Context 'Authenticating with vlab' {
+
+        BeforeEach {
+            #  ----------- obtain credentials -----------
+            Write-Verbose "looking for available *.cred file in $($PWD)..."
+            if ( -not ( Get-ChildItem -Recurse "*.cred" | Test-Path -PathType Leaf ) ) {
+                Get-Credential -Message "please enter valid credentials to use in tests:" |
+                Export-Clixml -Path ".\TestResources\$($_.UserName).cred"
+            }
+            $tempCredentials = (Get-ChildItem -Recurse "*.cred")[0]
+            Write-Verbose "Using $($tempCredentials) for this test's credentials."
+
+            #  -------- save variables globally --------
+            @{'ViServerAddress'           = 'vlabw1vc.nqeng.lab'
+            'BadViServerAddress' = 'https://MicroFocus.com'
+                'VlabCredentialsFilePath' = $tempCredentials 
+            }.GetEnumerator() | ForEach-Object { 
+                Set-Variable -Name $_.Key -Value $_.Value -Scope global -Visibility Public
+            }
+        }
+
+
+        it 'Connects to the vlab server with global variables' {
+
+            Join-Vlab
+            $global:DefaultVIServer.Name | Should -Be $ViServerAddress
+        }
+
+        it 'Connects to the vlab without prepped global variables' {
+            Write-Debug ([string]::Format("Available Variables: `n`t{0}`n`t{1}", 
+                    "VlabCredentialsFilePath: '$($VlabCredentialsFilePath)'",
+                    "ViServerAddress: '$($ViServerAddress)'"))
+
+        }
+    }
+}
